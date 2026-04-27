80482ecd7e51a49c87fc29a112be03fe1bea40b5
WYGIN <wygininc@gmail.com>
2026-04-27 10:48:42 +0530

    remove fshash package
    Signed-off-by: WYGIN <wygininc@gmail.com>

--- pkg/fshash/file_other.go
+++
@@ -1,8 +0,0 @@
-//go:build !linux
-
-package fshash
-
-import "os"
-
-func openForHash(absPath string, _ int64) (*os.File, error) { return os.Open(absPath) } //nolint:gosec
-func releasePageCache(_ *os.File, _ int64)                  {}
